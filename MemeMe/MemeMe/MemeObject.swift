e6d17ecb1e9641908c102bccac1b759ff63f4aeb
dgundry <80338563+dgundry@users.noreply.github.com>
2022-10-23 11:08:37 -0400

    Meme Update

---
+++ MemeMe/MemeMe/MemeObject.swift
@@ -0,0 +1,16 @@
+//
+//  MemeObject.swift
+//  MemeMe
+//
+//  Created by Devin Gundry on 10/21/22.
+//
+
+import Foundation
+import UIKit
+
+struct MemeObject{
+    var originalImage: UIImage!
+    var topText: String!
+    var bottomText: String!
+    var editedImage: UIImage!
+}
--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -8,74 +8,91 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
-
+    
     
     
     @IBOutlet weak var memeView: UIImageView!
-    @IBOutlet weak var shareButton: UIButton!
-    @IBOutlet weak var albumButton: UIButton!
-    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var toolBar: UIToolbar!
+    let memeTextAttributes = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "Impact", size: 45)!,
+        NSAttributedString.Key.strokeWidth: NSNumber(value: -3.0 as Float)
+    ]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //Image Selection
+    @IBAction func imageSelection(_ sender: UIBarButtonItem) {
+        if(sender == albumButton){
+            imagePickerController(sourceType: .photoLibrary)
+        }else{
+            imagePickerController(sourceType: .camera)
+        }
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-        shareButton.isEnabled = false
-    }
-    
-    /*
-     Share Button Section
-     */
-    @IBAction func shareAction(_ sender: Any) {
-        shareButton.isHidden = true
-        albumButton.isHidden = true
-        cameraButton.isHidden = true
-        let image = generateMemedImage()
-        shareButton.isHidden = false
-        albumButton.isHidden = false
-        cameraButton.isHidden = false
-        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-        present(controller, animated: true, completion: nil)
-    }
-    func generateMemedImage() -> UIImage {
-
-        // Render view to an image
-        UIGraphicsBeginImageContext(self.view.frame.size)
-        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
-        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-
-        return memedImage
-    }
-    /*
-     Camera Button Section
-     */
-    @IBAction func cameraAction(_ sender: Any) {
-        let cameraPicker = UIImagePickerController()
-        cameraPicker.delegate = self
-        cameraPicker.sourceType = .camera
-        present(cameraPicker, animated: true, completion: nil)
-    }
-    /*
-     Album Button Section
-     */
-    @IBAction func albumAction(_ sender: Any) {
-        let albumPicker = UIImagePickerController()
-        albumPicker.delegate = self
-        present(albumPicker, animated: true, completion: nil)
+    func imagePickerController(sourceType: UIImagePickerController.SourceType){
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = sourceType
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[.originalImage] as? UIImage{
+        if let image = info[.originalImage] as? UIImage {
             memeView.image = image
-            dismiss(animated: true, completion: nil)
             shareButton.isEnabled = true
         }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Share Button
+    @IBAction func shareAction(_ sender: Any) {
+        let memeImage = generateMemedImage()
+        let controller = UIActivityViewController(activityItems: [memeImage], applicationActivities: nil)
+        save(memeImage: memeImage)
+        present(controller, animated: true, completion: nil)
+    }
+    func generateMemedImage() -> UIImage {
+        navBar.isHidden = true
+        toolBar.isHidden = true
+        
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memeImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        navBar.isHidden = false
+        toolBar.isHidden = false
+        
+        return memeImage
+    }
+    func save(memeImage: UIImage) {
+        let meme = MemeObject(originalImage: memeView.image!, topText: topText.text!, bottomText: bottomText.text!, editedImage: memeImage)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        topText.defaultTextAttributes = memeTextAttributes;
+        bottomText.defaultTextAttributes = memeTextAttributes;
+        topText.textAlignment = .center
+        bottomText.textAlignment = .center
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = false //UIImagePickerController.isSourceTypeAvailable(.camera)
+        shareButton.isEnabled = false
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
     }
 }
+    
 
